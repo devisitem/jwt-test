22794d68def3e96b55bf009a64ad4915d3e7a403
Kimchi Developer <65699391+Kimchi-dev@users.noreply.github.com>
2021-06-14 08:27:24 +0900

    create project

---
+++ src/test/java/me/kimchi/JwtTest/JwtTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kimchi.JwtTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
