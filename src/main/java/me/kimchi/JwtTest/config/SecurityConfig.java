667a0ad55844f1cc66cd79c0be8538083ed9b3fc
Kimchi Developer <65699391+Kimchi-dev@users.noreply.github.com>
2021-06-16 22:33:51 +0900

    JWT Configuration

--- src/main/java/me/kimchi/JwtTest/config/SecurityConfig.java
+++ src/main/java/me/kimchi/JwtTest/config/SecurityConfig.java
@@ -1,12 +1,38 @@
 package me.kimchi.JwtTest.config;
 
+import lombok.RequiredArgsConstructor;
+import me.kimchi.JwtTest.jwt.JwtAccessDeniedHandler;
+import me.kimchi.JwtTest.jwt.JwtAuthenticationEntryPoint;
+import me.kimchi.JwtTest.jwt.JwtSecurityConfig;
+import me.kimchi.JwtTest.jwt.TokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+/*
+* @PreAuthorize를 method 단위로 추가하기 위해 사용
+* */
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     public void configure(WebSecurity web) throws Exception {
         web
@@ -19,9 +45,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
+
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+                .and()
+
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+                .and()
+
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
                 .authorizeRequests()
                 .antMatchers("/api/hello").permitAll()
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/signup").permitAll()
                 .anyRequest().authenticated()
+                .and()
+
+                .apply(new JwtSecurityConfig(tokenProvider))
         ;
     }
+
+
 }
