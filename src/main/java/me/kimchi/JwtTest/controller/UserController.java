328fbda9aa73a61121e5a0d3d2519e017c7537d1
Kimchi Developer <65699391+Kimchi-dev@users.noreply.github.com>
2021-06-20 22:18:33 +0900

    complete jwt authorization

---
+++ src/main/java/me/kimchi/JwtTest/controller/UserController.java
@@ -0,0 +1,37 @@
+package me.kimchi.JwtTest.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.kimchi.JwtTest.dto.UserDto;
+import me.kimchi.JwtTest.entity.User;
+import me.kimchi.JwtTest.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> signup(@Valid @RequestBody UserDto userDto){
+        return ResponseEntity.ok(userService.signup(userDto));
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<User> getMyUserInfo(){
+        return ResponseEntity.ok(userService.getMyUserWithAuthorities().get());
+    }
+
+    @GetMapping("/user/{username}")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity<User> getUserInfo(@PathVariable String username) {
+        return ResponseEntity.ok(userService.getUserWithAuthorities(username).get());
+    }
+}
